2da3afc02dc1685858b3ea153272b0a456d75755
aleksander.chrzanowski.dev <aleksander.chrzanowski.dev@gmail.com>
2022-11-18 13:06:37 +0100

    init commit

---
+++ src/main/java/pl/achrzanowski/moneymanagementcaptchaservice/MoneyManagementCaptchaServiceApplication.java
@@ -0,0 +1,13 @@
+package pl.achrzanowski.moneymanagementcaptchaservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoneyManagementCaptchaServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoneyManagementCaptchaServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/achrzanowski/moneymanagementcaptchaservice/MoneyManagementCaptchaServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.achrzanowski.moneymanagementcaptchaservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoneyManagementCaptchaServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
